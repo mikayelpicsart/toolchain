1f0d149d5802b6c42b2ca0b5a213f46e3c0f11a0
Mikayel Abrahamyan <ma@MikayelAbrahamyanQ2.local>
2020-03-14 16:54:56 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import { removeBackgroundBulk as removebackgroundBulk } from "./effects/removeBackground";
-import { runOut as runout } from "./effects/runOut";
 
 export const removeBackgroundBulk = removebackgroundBulk;
-export const runOut = runout;
